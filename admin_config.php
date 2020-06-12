5ce1e6f9d602d4ed9e789730acd37baeebca1775
Moc <github@tijnkuyper.nl>
2020-06-12 10:44:46 +0200

    Basic Admin UI done

--- admin_config.php
+++ admin_config.php
@@ -22,17 +22,31 @@ class events2bots_adminArea extends e_admin_dispatcher
         ),
 
         'user' => array(
-            'controller'    => 'e2b_user_eventrules_ui',
+            'controller'    => 'e2b_eventrules_ui',
             'path'          => null,
             'ui'            => 'e2b_user_eventrules_form_ui',
             'uipath'        => null
         ),
 
         'news' => array(
-            'controller' 	=> 'e2b_news_eventrules_ui',
+            'controller' 	=> 'e2b_eventrules_ui',
             'path'			=> null,
             'ui' 			=> 'e2b_news_eventrules_form_ui',
             'uipath' 		=> null
+        ),
+
+        'forum' => array(
+            'controller'    => 'e2b_eventrules_ui',
+            'path'          => null,
+            'ui'            => 'e2b_forum_eventrules_form_ui',
+            'uipath'        => null
+        ),
+
+        'chatbox' => array(
+            'controller'    => 'e2b_eventrules_ui',
+            'path'          => null,
+            'ui'            => 'e2b_chatbox_eventrules_form_ui',
+            'uipath'        => null
         ),
 
     );
@@ -78,13 +92,38 @@ class events2bots_adminArea extends e_admin_dispatcher
             'divider' => true
         ),
 
+        'forum/list' => array(
+            'caption'   => "Manage Forum event rules",
+            'perm'      => 'P'
+        ),
+
+        'forum/create' => array(
+            'caption'   => "Create Forum event rules",
+            'perm'      => 'P'
+        ),
+
+        'main/div3' => array(
+            'divider' => true
+        ),
+
+        'chatbox/list' => array(
+            'caption'   => "Manage Chatbox event rules",
+            'perm'      => 'P'
+        ),
+
+        'chatbox/create' => array(
+            'caption'   => "Create Chatbox event rules",
+            'perm'      => 'P'
+        ),
+
+        'main/div4' => array(
+            'divider' => true
+        ),
+
         'main/prefs' => array(
             'caption' 	=> LAN_PREFS,
             'perm' 		=> 'P'
         ),
-
-        // 'main/div0'      => array('divider'=> true),
-        // 'main/custom'		=> array('caption'=> 'Custom Page', 'perm' => 'P'),
         
     );
 
@@ -335,34 +374,15 @@ class e2b_bots_ui extends e_admin_ui
             'caption' 	=> $caption,
             'text' 		=> $text
         );
-
     }
-
-    /*
-    // optional - a custom page.
-    public function customPage()
-    {
-    $text = 'Hello World!';
-    $otherField  = $this->getController()->getFieldVar('other_field_name');
-    return $text;
-    
-    }
-    
-    
-    
-    
-    */
-
 }
 
 class e2b_bots_form_ui extends e_admin_form_ui
 {
-
 }
 
-class e2b_user_eventrules_ui extends e_admin_ui
+class e2b_eventrules_ui extends e_admin_ui
 {
-
     protected $pluginTitle 	= 'Events2Bots';
     protected $pluginName 	= 'events2bots';
     //	protected $eventName		= 'events2bots-e2b_eventrules'; // remove comment to enable event triggers in admin.
@@ -378,14 +398,14 @@ class e2b_user_eventrules_ui extends e_admin_ui
     //	protected $treePrefix      = 'somefield_title';
     //	protected $tabs				= array('Tabl 1','Tab 2'); // Use 'tab'=>0  OR 'tab'=>1 in the $fields below to enable.
 
-    protected $listQry      	= 
+    /*protected $listQry      	= 
     "
         SELECT er.*, b.bot_avatar
         FROM `#e2b_eventrules` as er
         LEFT JOIN `#e2b_bots` as b
         ON er.er_botid = b.bot_id
         WHERE (er.er_eventname = 'user_signup_submitted' OR er.er_eventname = 'user_signup_activated')
-    "; 
+    ";*/
 
     protected $listOrder = 'er_id ASC';
 
@@ -509,7 +529,7 @@ class e2b_user_eventrules_ui extends e_admin_ui
             e107::getMessage()->addWarning("This plugin is not yet installed. Saving and loading of preference or table data will fail.");
         }
 
-        // Bots
+        // Bots selection
         $this->er_botid[0] = "Select bot...";
         if(e107::getDb()->select('e2b_bots'))
         {
@@ -521,24 +541,91 @@ class e2b_user_eventrules_ui extends e_admin_ui
 
         $this->fields['er_botid']['writeParms'] = $this->er_botid; 
 
-        
-        
-        // Events
-        $user_events = array(
-            "user_signup_submitted" => "New user registration",
-            "user_signup_activated" => "New user has activated their account",
-        );
-        
-        //print_a($user_events);
+/*
 
-        //$this->er_eventname[0] = "Select event...";
+            "forum" => array(
+                "user_forum_topic_created",
+                //"user_forum_topic_updated",
+                "user_forum_post_created", 
+            ),
+            "chatbox" => array(
+                "user_chatbox_post_created",
+            ),
+            */
+        
+        // Set $listQry and $events array
+        switch ($this->getMode()) 
+        {
+            case 'user':
+                    $events = array(
+                        "user_signup_submitted" => "New user registration",
+                        "user_signup_activated" => "New user has activated their account",
+                    );
 
-        foreach($user_events as $id => $value)
+                    $this->listQry = 
+                        "SELECT er.*, b.bot_avatar
+                        FROM `#e2b_eventrules` as er
+                        LEFT JOIN `#e2b_bots` as b
+                        ON er.er_botid = b.bot_id
+                        WHERE (er.er_eventname = 'user_signup_submitted' OR er.er_eventname = 'user_signup_activated')
+                        ";
+                break;
+            case 'news':
+                    $events = array(
+                        "admin_news_create" => "New news item posted",
+                        "admin_news_update" => "Existing news item updated",
+                    );
+
+                    $this->listQry = 
+                        "SELECT er.*, b.bot_avatar
+                        FROM `#e2b_eventrules` as er
+                        LEFT JOIN `#e2b_bots` as b
+                        ON er.er_botid = b.bot_id
+                        WHERE (er.er_eventname = 'admin_news_create' OR er.er_eventname = 'admin_news_update')
+                        ";
+                break;
+            case 'forum':
+                    $events = array(
+                        "user_forum_topic_created" => "New forum topic created",
+                        "user_forum_post_created"  => "New forum post created",
+                    );
+
+                    $this->listQry = 
+                        "SELECT er.*, b.bot_avatar
+                        FROM `#e2b_eventrules` as er
+                        LEFT JOIN `#e2b_bots` as b
+                        ON er.er_botid = b.bot_id
+                        WHERE (er.er_eventname = 'user_forum_topic_created' OR er.er_eventname = 'user_forum_post_created')
+                        ";
+                break;
+            case 'chatbox':
+                    $events = array(
+                        "user_chatbox_post_created" => "New chatbox message posted",
+                    );
+
+                    $this->listQry = 
+                        "SELECT er.*, b.bot_avatar
+                        FROM `#e2b_eventrules` as er
+                        LEFT JOIN `#e2b_bots` as b
+                        ON er.er_botid = b.bot_id
+                        WHERE (er.er_eventname = 'user_chatbox_post_created')
+                        ";
+                break;
+            default:
+                # code...
+                break;
+        }
+        
+        // Set er_eventname column 
+        foreach($events as $id => $value)
         {
             $this->er_eventname[$id] = $value; 
         }
 
         $this->fields['er_eventname']['writeParms']['optArray'] = $this->er_eventname; 
+
+
+        // Check modes and hide 'er_sections'
 
         
     }
@@ -596,333 +683,21 @@ class e2b_user_eventrules_ui extends e_admin_ui
 
 class e2b_user_eventrules_form_ui extends e_admin_form_ui
 {
-
-    // Custom Method/Function
-    function er_eventname($curVal, $mode)
-    {
-        $user_events = array(
-            "user_signup_submitted" => "New user registration",
-            "user_signup_activated" => "New user has activated their account",
-        );
-
-        switch ($mode)
-        {
-            case 'read': // List Page
-                return $user_events[$curVal]. " (".$curVal.")";
-            break;
-            case 'write': // Edit Page
-                return $this->text('er_selectedevents', $curVal, 255, 'size=large');
-            break;
-            case 'filter':
-            break;
-            case 'batch':
-            break;
-        }
-
-        return null;
-    }
-
-    // Custom Method/Function
-    function er_botid($curVal, $mode)
-    {
-        $user_events = array(
-            "user_signup_submitted" => "New user registration",
-            "user_signup_activated" => "New user has activated their account",
-        );
-
-        switch ($mode)
-        {
-            case 'read': // List Page
-                return $curVal;
-            break;
-            case 'write': // Edit Page
-                //return $this->text('er_selectedevents', $curVal, 255, 'size=large');
-                return "dropdown here";
-            break;
-            case 'filter':
-            break;
-            case 'batch':
-            break;
-        }
-
-        return null;
-    }
-
 }
 
-class e2b_news_eventrules_ui extends e_admin_ui
-{
-
-    protected $pluginTitle  = 'Events2Bots';
-    protected $pluginName   = 'events2bots';
-    //  protected $eventName        = 'events2bots-e2b_eventrules'; // remove comment to enable event triggers in admin.
-    protected $table        = 'e2b_eventrules';
-    protected $pid          = 'er_id';
-    protected $perPage      = 10;
-    protected $batchDelete  = true;
-    protected $batchExport  = true;
-    protected $batchCopy    = true;
-
-    //  protected $sortField        = 'somefield_order';
-    //  protected $sortParent      = 'somefield_parent';
-    //  protected $treePrefix      = 'somefield_title';
-    //  protected $tabs             = array('Tabl 1','Tab 2'); // Use 'tab'=>0  OR 'tab'=>1 in the $fields below to enable.
-    //  protected $listQry          = "SELECT * FROM `#tableName` WHERE field != '' "; // Example Custom Query. LEFT JOINS allowed. Should be without any Order or Limit.
-    protected $listOrder = 'er_id DESC';
-
-    protected $fields = array(
-        'checkboxes' => array(
-            'title' => '',
-            'type' => null,
-            'data' => null,
-            'width' => '5%',
-            'thclass' => 'center',
-            'forced' => true,
-            'class' => 'center',
-            'toggle' => 'e-multiselect',
-            'readParms' => array(),
-            'writeParms' => array(),
-        ) ,
-        'er_id' => array(
-            'title' => LAN_ID,
-            'data' => 'int',
-            'width' => '5%',
-            'help' => '',
-            'readParms' => array(),
-            'writeParms' => array(),
-            'class' => 'left',
-            'thclass' => 'left',
-        ) ,
-        'er_name' => array(
-            'title' => LAN_TITLE,
-            'type' => 'text',
-            'data' => 'int',
-            'width' => 'auto',
-            'inline' => true,
-            'help' => '',
-            'readParms' => array(),
-            'writeParms' => array(),
-            'class' => 'left',
-            'thclass' => 'left',
-        ) ,
-        'er_selectedevents' => array(
-            'title' => 'Selectedevents',
-            'type' => 'method',
-            'data' => 'int',
-            'width' => 'auto',
-            'help' => '',
-            'readParms' => array(),
-            'writeParms' => array(),
-            'class' => 'left',
-            'thclass' => 'left',
-            'filter' => false,
-            'batch' => false,
-        ) ,
-        'er_sections' => array(
-            'title' => 'Sections',
-            'type' => 'dropdown',
-            'data' => 'int',
-            'width' => 'auto',
-            'help' => '',
-            'readParms' => array(),
-            'writeParms' => array(),
-            'class' => 'left',
-            'thclass' => 'left',
-            'filter' => false,
-            'batch' => false,
-        ) ,
-        'options' => array(
-            'title' => LAN_OPTIONS,
-            'type' => null,
-            'data' => null,
-            'width' => '10%',
-            'thclass' => 'center last',
-            'class' => 'center last',
-            'forced' => true,
-            'readParms' => array(),
-            'writeParms' => array(),
-        ) ,
-    );
-
-    protected $fieldpref = array(
-        'er_name'
-    );
-
-    public function init()
-    {
-        // This code may be removed once plugin development is complete.
-        if (!e107::isInstalled('events2bots'))
-        {
-            e107::getMessage()->addWarning("This plugin is not yet installed. Saving and loading of preference or table data will fail.");
-        }
-
-        // Set drop-down values (if any).
-        $this->fields['er_sections']['writeParms']['optArray'] = array(
-            'er_sections_0',
-            'er_sections_1',
-            'er_sections_2'
-        ); // Example Drop-down array.
-        
-    }
-
-    // ------- Customize Create --------
-    public function beforeCreate($new_data, $old_data)
-    {
-        return $new_data;
-    }
-
-    public function afterCreate($new_data, $old_data, $id)
-    {
-        // do something
-        
-    }
-
-    public function onCreateError($new_data, $old_data)
-    {
-        // do something
-        
-    }
-
-    // ------- Customize Update --------
-    public function beforeUpdate($new_data, $old_data, $id)
-    {
-        return $new_data;
-    }
-
-    public function afterUpdate($new_data, $old_data, $id)
-    {
-        // do something
-        
-    }
-
-    public function onUpdateError($new_data, $old_data, $id)
-    {
-        // do something
-        
-    }
-
-    // left-panel help menu area. (replaces e_help.php used in old plugins)
-    public function renderHelp()
-    {
-        $caption = LAN_HELP;
-        $text = 'Some help text';
-
-        return array(
-            'caption'   => $caption,
-            'text'      => $text
-        );
-
-    }
-
-    /*
-    // optional - a custom page.
-    public function customPage()
-    {
-    $text = 'Hello World!';
-    $otherField  = $this->getController()->getFieldVar('other_field_name');
-    return $text;
-    
-    }
-    
-    
-    
-    // Handle batch options as defined in e2b_eventrules_form_ui::er_selectedevents;  'handle' + action + field + 'Batch'
-    // @important $fields['er_selectedevents']['batch'] must be true for this method to be detected.
-    // @param $selected
-    // @param $type
-    function handleListErSelectedeventsBatch($selected, $type)
-    {
-    
-    $ids = implode(',', $selected);
-    
-    switch($type)
-    {
-    case 'custombatch_1':
-    // do something
-    e107::getMessage()->addSuccess('Executed custombatch_1');
-    break;
-    
-    case 'custombatch_2':
-    // do something
-    e107::getMessage()->addSuccess('Executed custombatch_2');
-    break;
-    
-    }
-    
-    
-    }
-    
-    
-    // Handle filter options as defined in e2b_eventrules_form_ui::er_selectedevents;  'handle' + action + field + 'Filter'
-    // @important $fields['er_selectedevents']['filter'] must be true for this method to be detected.
-    // @param $selected
-    // @param $type
-    function handleListErSelectedeventsFilter($type)
-    {
-    
-    $this->listOrder = 'er_selectedevents ASC';
-    
-    switch($type)
-    {
-    case 'customfilter_1':
-    // return ' er_selectedevents != 'something' '; 
-    e107::getMessage()->addSuccess('Executed customfilter_1');
-    break;
-    
-    case 'customfilter_2':
-    // return ' er_selectedevents != 'something' '; 
-    e107::getMessage()->addSuccess('Executed customfilter_2');
-    break;
-    
-    }
-    
-    
-    }
-    
-    
-    
-    */
-
-}
 
 class e2b_news_eventrules_form_ui extends e_admin_form_ui
 {
-
-    // Custom Method/Function
-    function er_selectedevents($curVal, $mode)
-    {
-
-
-
-        switch ($mode)
-        {
-            case 'read': // List Page
-                return $curVal;
-            break;
-
-            case 'write': // Edit Page
-                return $this->text('er_selectedevents', $curVal, 255, 'size=large');
-            break;
-
-            case 'filter':
-                return array(
-                    'customfilter_1' => 'Custom Filter 1',
-                    'customfilter_2' => 'Custom Filter 2'
-                );
-            break;
-
-            case 'batch':
-                return array(
-                    'custombatch_1' => 'Custom Batch 1',
-                    'custombatch_2' => 'Custom Batch 2'
-                );
-            break;
-        }
-
-        return null;
-    }
-
 }
+
+class e2b_forum_eventrules_form_ui extends e_admin_form_ui
+{
+}
+
+class e2b_chatbox_eventrules_form_ui extends e_admin_form_ui
+{
+}
+
 
 new events2bots_adminArea();
 
